f82efcc29f3a51e6a1ef7b363a881049e07322f4
Sheshnath <yadavsheshnath236@gmail.com>
2026-04-13 13:29:35 +0530

    localhost link nahi hai  controller emai id add

--- backend/controllers/starExportHouse.controller.js
+++ backend/controllers/starExportHouse.controller.js
@@ -39,7 +39,7 @@ async function sendEmail(record) {
 
   await transporter.sendMail({
     from: `"EXIMINQ CloudDesk" <${process.env.SMTP_USER}>`,
-    to: " yadavsheshnath236@gmail.com",
+    to: "crm@eximinq.com, omkarmhetar100@gmail.com, yadavsheshnath236@gmail.com",
     subject: `star-export-house — ${serviceDisplay}`,
     html: `
       <div style="font-family:Arial;">
