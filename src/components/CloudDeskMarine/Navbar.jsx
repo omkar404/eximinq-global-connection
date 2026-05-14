9bffd6fe398920c99927fcb4ee87132ff59c8d8d
Sheshnath <yadavsheshnath236@gmail.com>
2026-05-14 18:01:09 +0530

    marine-insurance new chnage

--- src/components/CloudDeskMarine/Navbar.jsx
+++ src/components/CloudDeskMarine/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ setShowEnrollModal }) => {
         <div className="hidden md:flex gap-10 font-bold text-sm text-slate-700">
           <a href="#home" class="hover:text-brand-600 transition">
             Home
-          </a> 
+          </a>
           <a href="#about" class="hover:text-brand-600 transition">
             Why Insure?
           </a>
@@ -29,26 +29,22 @@ const Navbar = ({ setShowEnrollModal }) => {
         </div>
 
         {/* Apply Now Button - STILL RIGHT ALIGNED (optional) */}
-        <button
-          onClick={() =>
-            setShowEnrollModal({
-              open: true,
-              actionType: "Apply Now",
-              source: "services/epcg-scheme",
-            })
-          }
-          className="hidden md:inline-block bg-accent-500 hover:bg-accent-600 text-white font-bold py-2 px-6 rounded-md shadow-md absolute right-4"
-        >
-          Apply Now
-        </button>
 
-{/* <button
-  onClick={() => setShowEnrollModal({open:true , type: "Buy_Policy"})}
-  className="hidden md:inline-block bg-accent-500 hover:bg-accent-600 
-             text-white font-bold py-2 px-6 rounded-md shadow-md absolute right-4"
->
-  Buy Policy
-</button> */}
+          {/* <button
+            onClick={() => setShowEnrollModal({ open: true, type: "Buy_Policy" })}
+            className="hidden md:inline-block bg-accent-500 hover:bg-accent-600 
+              text-white font-bold py-2 px-6 rounded-md shadow-md absolute right-4"
+          >
+            Buy Policy
+          </button> */}
+
+        <a 
+          href="#home"
+          className="hidden md:inline-block bg-accent-500 hover:bg-accent-600 
+              text-white font-bold py-2 px-6 rounded-md shadow-md absolute right-4"
+        >
+          Buy Policy
+        </a>
 
         {/* <button
         href=
