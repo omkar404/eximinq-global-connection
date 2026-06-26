e9945313be94e62bd35b2b5cf4253c293452544b
Sheshnath <yadavsheshnath236@gmail.com>
2026-06-26 17:10:07 +0530

    svb-registration add the navbar

--- src/components/CloudDeskSVB/Navbar.jsx
+++ src/components/CloudDeskSVB/Navbar.jsx
@@ -10,11 +10,12 @@ const Navbar = ({
       <nav className="container mx-auto px-4 py-4 flex justify-center items-center">
         {/* CENTERED MENU */}
         <div className="hidden md:flex gap-10 font-bold text-sm text-slate-700">
-          <a href="#home" class="hover:text-brand-600 transition">Home</a>
+                <a href="#home" class="hover:text-brand-600 transition">Home</a>
                 <a href="#about" class="hover:text-brand-600 transition">What is SVB?</a>
                 <a href="#related" class="hover:text-brand-600 transition">Related Party Check</a>
                 <a href="#process" class="hover:text-brand-600 transition">Process</a>
-                <a href="#contact" class="hover:text-brand-600 transition">Renewal</a>
+                <a href="#renewal" class="hover:text-brand-600 transition">Renewal</a>
+                <a href="#contact" class="hover:text-brand-600 transition">Fees</a>
         </div>
 
         {/* Apply Now Button - STILL RIGHT ALIGNED (optional) */}
