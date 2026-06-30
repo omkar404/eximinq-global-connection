d78132013c691d6c9f8ffc1f07e27aedcdc8677a
Sheshnath <yadavsheshnath236@gmail.com>
2026-06-30 13:47:56 +0530

    dfia-license navbar change the

--- src/components/CloudDeskDFIA/Navbar.jsx
+++ src/components/CloudDeskDFIA/Navbar.jsx
@@ -10,12 +10,13 @@ const Navbar = ({
       <nav className="container mx-auto px-4 py-4 flex justify-center items-center">
         {/* CENTERED MENU */}
         <div className="hidden md:flex gap-10 font-bold text-sm text-slate-700">
-          <a href="#home" class="hover:text-brand-600 transition">Home</a>
+                <a href="#home" class="hover:text-brand-600 transition">Home</a>
                 <a href="#about" class="hover:text-brand-600 transition">What is DFIA?</a>
                 <a href="#eligibility" class="hover:text-brand-600 transition">Eligibility</a>
                 <a href="#benefits" class="hover:text-brand-600 transition">Benefits</a>
                 <a href="#documents" class="hover:text-brand-600 transition">Documents</a>
                 <a href="#process" class="hover:text-brand-600 transition">Process</a>
+                <a href="#references" class="hover:text-brand-600 transition">References</a>
                 <a href="#contact" class="hover:text-brand-600 transition">Fees</a>
         </div>
 
