11072da121414cfa7196fd9b19b0b43d6ea8d9c5
Sheshnath <yadavsheshnath236@gmail.com>
2026-07-20 15:55:17 +0530

    iso cetificate new quick form change

--- backend/controllers/isoCertificationRoutes.controller.js
+++ backend/controllers/isoCertificationRoutes.controller.js
@@ -28,6 +28,8 @@ async function sendEmail(record) {
     issue,
     standard,
     accreditation,
+    companyName,
+    personName
   } = record;
 
   const serviceDisplay = service || "ISO Registration";
@@ -44,6 +46,8 @@ async function sendEmail(record) {
           <tr><td><b>Service</b></td><td>${serviceDisplay}</td></tr>
           ${standard ? `<tr><td><b>ISO Standard</b></td><td>${standard}</td></tr>` : ""}
           ${accreditation ? `<tr><td><b>Accreditation Type</b></td><td>${accreditation}</td></tr>` : ""}
+          ${companyName ? `<tr><td><b>Company Name</b></td><td>${companyName}</td></tr>` : ""}
+          ${personName ? `<tr><td><b>Contact Person Name</b></td><td>${personName}</td></tr>` : ""}
           ${category ? `<tr><td><b>Category</b></td><td>${category}</td></tr>` : ""}
           ${issue ? `<tr><td><b>Issue</b></td><td>${issue}</td></tr>` : ""}
           <tr><td><b>Mobile</b></td><td>${mobile}</td></tr>
@@ -79,6 +83,8 @@ exports.createisoCertificationRoutes = async (req, res) => {
       issue,
       standard, // ✅ camelCase
       accreditation, // ✅ camelCase
+      companyName,
+      personName,
     } = req.body;
 
     const isQuickForm = type === "QUICK_FORM";
@@ -95,6 +101,8 @@ exports.createisoCertificationRoutes = async (req, res) => {
       mobile: mobile.trim(),
       standard: standard ? standard.trim() : null, // ✅ use the correct variable
       accreditation: accreditation ? accreditation.trim() : null, // ✅ use correct variable
+      companyName : companyName ? companyName.trim() : null,
+      personName : personName ? personName.trim() : null,
       name: isQuickForm ? null : name ? name.trim() : null,
       email: isQuickForm ? null : email ? email.trim().toLowerCase() : null,
       entity: isQuickForm ? null : entity ? entity.trim() : null,
